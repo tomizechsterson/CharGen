4ab13ef5c95998a4cb3e61cd3c35ed0d1c1bba02
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2017-03-23 10:23:19 -0700

    Fixing solution
    Removing general project and restarting with better-named services

---
+++ CharGen.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CharGen.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert(false);
+        }
+    }
+}
