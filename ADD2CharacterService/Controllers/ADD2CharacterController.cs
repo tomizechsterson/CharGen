c5e75b1433fc1204441d4a6fe7e1e3431a431b58
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2017-05-26 15:00:11 -0700

    ADD2 Character progress
    Renamed controller and added tests for the model object

--- ADD2CharacterService/Controllers/ADD2CharacterController.cs
+++ ADD2CharacterService/Controllers/ADD2CharacterController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ADD2CharacterService.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class ADD2CharacterController : Controller
     {
         // GET api/values
         [HttpGet]
---
+++ ADD2CharacterService/Model/Add2CharacterUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace ADD2CharacterService.Model
+{
+    public class ADD2CharacterUnitTests
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            var character = new ADD2Character();
+
+            string value = character.ToString();
+
+            Assert.Equal("", value);
+        }
+    }
+
+    public class ADD2Character
+    {
+    }
+}
