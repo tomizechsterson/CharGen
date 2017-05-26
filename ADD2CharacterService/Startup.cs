2f1448e1986c89fe0fec74daa9d084e102d33fb2
Tomas Eychaner <tomas.eychaner@extendhealth.com>
2017-05-26 14:44:33 -0700

    Adding first character type service
    Some code being temporarily stored in Startup.cs

---
+++ ADD2CharacterService/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ADD2CharacterService
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+
+
+            using (var connection = new SqliteConnection(new SqliteConnectionStringBuilder {DataSource = "DB/test.db"}.ToString()))
+            {
+                var command = connection.CreateCommand();
+                command.CommandText = "CREATE TABLE testTable (Id INT, Name VARCHAR(32))";
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
