7c1784cf0ccd354d66076f78a2726bddf6112c62
Tomas <tomizechsterson@gmail.com>
2018-05-03 23:30:43 -0700

    This seems to be a better way to handle exceptions

---
+++ ADD2CharacterService/ExceptionHandling/GlobalExceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ADD2CharacterService.ExceptionHandling
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            HttpStatusCode status;
+            string message;
+ 
+            var exceptionType = context.Exception.GetType();
+            if (exceptionType == typeof(ArgumentOutOfRangeException))
+            {
+                message = context.Exception.Message;
+                status = HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                message = context.Exception.Message;
+                status = HttpStatusCode.InternalServerError;
+            }
+            
+            context.ExceptionHandled = true;
+ 
+            var response = context.HttpContext.Response;
+            response.StatusCode = (int)status;
+            response.ContentType = "application/text";
+            response.WriteAsync(message);
+        }
+    }
+}
