0d32ab7720d41ac8611c52a073936968819205ac
Tomas <tomizechsterson@gmail.com>
2018-05-16 21:34:15 -0700

    Create new D&D 3.5 character service
    Resolves #33

---
+++ DD35CharacterService/Controllers/DD35CharacterController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DD35CharacterService.Controllers
+{
+    [Route("api/[controller]")]
+    public class DD35CharacterController : Controller
+    {
+        
+    }
+}
---
+++ DD35CharacterService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DD35CharacterService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        private static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:42001")
+                .Build();
+    }
+}
