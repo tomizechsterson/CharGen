da35d97795cc068e8cd3119fdb5913a96daa7b81
Tomas <tomizechsterson@gmail.com>
2018-05-16 22:25:33 -0700

    Create method to test deployment

--- DD35CharacterService/Controllers/DD35CharacterController.cs
+++ DD35CharacterService/Controllers/DD35CharacterController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DD35CharacterService.Controllers
 {
     [Route("api/[controller]")]
     public class DD35CharacterController : Controller
     {
-        
+        [EnableCors("AnyOrigin")]
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "HAI!";
+        }
     }
 }
