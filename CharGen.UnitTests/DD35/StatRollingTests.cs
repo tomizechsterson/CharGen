3e0c6b88d14c32557dd25fd0410e448067201aa2
Tomas <tomizechsterson@gmail.com>
2018-05-17 21:19:51 -0700

    Add new test for DD35 stat rolling

---
+++ CharGen.UnitTests/DD35/StatRollingTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharGen.UnitTests.DD35
+{
+    public class StatRollingTests
+    {
+        [Fact]
+        public void StatRolls()
+        {
+            var results = new DD35StatRoll().RollStats();
+
+//            Assert.Equal(6, results.Count);
+//            Assert.True(results.All(r => r.Length == 4));
+//            Assert.True(results.All(r => r.Sum() >= 4 && r.Sum() <= 24));
+        }
+    }
+
+    public class DD35StatRoll
+    {
+        public List<int[]> RollStats()
+        {
+            return new List<int[]>();
+        }
+    }
+}
