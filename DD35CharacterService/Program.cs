02c9275511f5dfae9463139481256b8fd506cb2d
Tomas <tomizechsterson@gmail.com>
2018-09-07 13:55:57 -0700

    Make main Program class static (DD35 service)

--- DD35CharacterService/Program.cs
+++ DD35CharacterService/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace DD35CharacterService
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
