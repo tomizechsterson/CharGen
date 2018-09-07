b6ecf3c9de51d5566fe56eb381b08ea1468e814b
Tomas <tomizechsterson@gmail.com>
2018-09-07 13:44:04 -0700

    Try removing unused args from Main in ADD2 service

--- ADD2CharacterService/Program.cs
+++ ADD2CharacterService/Program.cs
@@ -5,7 +5,7 @@ namespace ADD2CharacterService
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
