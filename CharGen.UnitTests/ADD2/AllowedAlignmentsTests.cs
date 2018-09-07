6ed12de73f06f4e140ebefb31e32a734a428d977
Tomas <tomizechsterson@gmail.com>
2018-09-07 11:13:04 -0700

    Cover AllowedAlignments
    Don't remember why I neglected this

---
+++ CharGen.UnitTests/ADD2/AllowedAlignmentsTests.cs
@@ -0,0 +1,22 @@
+using ADD2CharacterService.App;
+using Xunit;
+
+namespace CharGen.UnitTests.ADD2
+{
+    public class AllowedAlignmentsTests
+    {
+        [Theory]
+        [InlineData("Paladin", new [] {"Lawful Good"})]
+        [InlineData("Ranger", new[] {"Lawful Good", "Neutral Good", "Chaotic Good"})]
+        [InlineData("Bard", new[] {"Lawful Neutral", "Neutral Good", "True Neutral", "Neutral Evil", "Chaotic Neutral"})]
+        [InlineData("Fighter", new[] {"Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral",
+            "True Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"})]
+        public void AllowedAlignmentsForClass(string className, string[] expectedAlignments)
+        {
+            var alignments = new AllowedAlignments(className).Get();
+
+            Assert.Equal(expectedAlignments.Length, alignments.Length);
+            Assert.Equal(expectedAlignments, alignments);
+        }
+    }
+}
