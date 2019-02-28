aebb9b9cc8fbd81d859796ba558aef955ee531be
Tomas <tomizechsterson@users.noreply.github.com>
2019-02-27 21:05:05 -0700

    Lots of version updates

--- ADD2CharacterService/Startup.cs
+++ ADD2CharacterService/Startup.cs
@@ -41,8 +41,6 @@ namespace ADD2CharacterService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-
             app.UseCors("AnyOrigin");
             app.UseMvc();
 
